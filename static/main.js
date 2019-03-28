316a5c65a742814406cd369348faa8a7c6b049d5
Anna <panova2006_89@mail.ru>
2019-03-28 20:51:26 +0300

    fixed bugs

--- static/main.js
+++ static/main.js
@@ -1,134 +1,133 @@
 class Profile {
     constructor({
-    username: username,
-    name: {firstName: firstName, lastName: lastName},
-    password: password
-    }){
-    this.username = username;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.password = password;
+                 username: username,
+                 name: {firstName: firstName, lastName: lastName},
+                 password: password
+               }){
+      this.username = username;
+      this.name = {
+          firstName,
+          lastName,
+      };
+      this.password = password;
     }
      
     createUser(callback) {
-    return ApiConnector.createUser({
-    username: this.username,
-    name: {firstName: this.firstName, lastName: this.lastName},
-    password: this.password
-    },(err,data) => {
-    console.log(`Has been created user ${this.username} , password ${this.password}`);
-    callback(err,data);
-    });
+      return ApiConnector.createUser({
+        username: this.username,
+        name: {firstName: this.firstName, lastName: this.lastName},
+        password: this.password
+      },(err,data) => {
+        console.log(`Has been created user ${this.username} , password ${this.password}`);
+        callback(err,data);
+      });
     }
      
     addMoney({ currency, amount }, callback) {
-    return ApiConnector.addMoney({ currency, amount }, (err, data) => {
-    console.log(`Adding ${amount} of ${currency} to ${this.username}`);
-    callback(err, data);
-    });
+      return ApiConnector.addMoney({ currency, amount }, (err, data) => {
+        console.log(`Adding ${amount} of ${currency} to ${this.username}`);
+        callback(err, data);
+      });
     }
      
     performLogin(callback) {
-    return ApiConnector.performLogin({username: this.username, password: this.password}, (err,data) => {
-    console.log(`Login: ${this.username} password:${this.password}`);
-    callback(err, data);
-    });
+      return ApiConnector.performLogin({username: this.username, password: this.password}, (err,data) => {
+        console.log(`Login: ${this.username} password:${this.password}`);
+        callback(err, data);
+      });
     }
      
     convertMoney({ fromCurrency, targetCurrency, targetAmount }, callback) {
-    return ApiConnector.convertMoney({fromCurrency,targetCurrency,targetAmount}, (err,data) => {
-    console.log(data,err);
-    console.log(`Convert from ${fromCurrency} to ${targetCurrency}. Amount = ${targetAmount}`);
-    callback(err,data);
-    });
+      return ApiConnector.convertMoney({fromCurrency,targetCurrency,targetAmount}, (err,data) => {
+        console.log(`Convert from ${fromCurrency} to ${targetCurrency}. Amount = ${targetAmount}`);
+        callback(err,data);
+      });
     }
      
     transferMoney({to,amount}, callback){
-    return ApiConnector.transferMoney({to,amount}, (err,data) => {
-    console.log(`Transfer ${amount} to ${to} from ${this.username}`);
-    callback(err,data);
-    });
+      return ApiConnector.transferMoney({to,amount}, (err,data) => {
+        console.log(`Transfer ${amount} to ${to} from ${this.username}`);
+        callback(err,data);
+      });
     }
      
-    getStocks(callback) {
-    return ApiConnector.getStocks((err,data)=>{
-    console.log(data);
-    console.log('GetStocks method is working');
-    callback(err,data);
-    });
-    }
+       
+}
+
+function getStocks(callback) {
+     return ApiConnector.getStocks((err,data)=>{
+       console.log(data);
+       console.log('GetStocks method is working');
+       callback(err,data);
+     });
+}
+
+
      
-    currentCurrency(){
-    ApiConnector.getStocks
-     
-    }
-     
-    }
-     
-    function main(){
+function main(){
      
     const Petya = new Profile({
-    username: 'petya',
-    name: { firstName: 'Petr', lastName: 'Ivanov' },
-    password: 'petyaspass'
+      username: 'petya',
+      name: { firstName: 'Petr', lastName: 'Ivanov' },
+      password: 'petyaspass'
     });
      
     Petya.createUser((err,data) =>{
-    if (err){
-    console.error('Error during creating user Petya')
-    }else{
-    console.log(`User Petya has been created`);
-    Petya.performLogin((err,data)=> {
-    if (err) {
-    console.error('Error during login user Petya')
-    }else{
-    console.log(`User Petya has been logined`);
+      if (err){
+        console.error('Error during creating user Petya')
+      }else{
+        console.log(`User Petya has been created`);
+        Petya.performLogin((err,data)=> {
+          if (err) {
+            console.error('Error during login user Petya')
+          }else{
+            console.log(`User Petya has been logined`);
     
-    }
-    });
-    }
+          }
+        });
+      }
     });
 
     const Ivan = new Profile({
          username: 'ivan',
          name: { firstName: 'Ivan', lastName: 'Petrov' },
          password: 'ivanspass'
-         });
+     });
      
     
     Ivan.createUser((err,data) =>{
-    if (err) {
-    console.error('Error during creating user Ivan')
-    }else{
-    console.log(`User Ivan has been created`);
-    Ivan.performLogin((err,data)=> {
-    if (err) {
-    console.error('Error during login user Ivan');
-    } else {
-    console.log(`User Ivan has been logined`);
-    Ivan.addMoney({ currency:'EUR', amount: 500000 }, (err, data) => {
-    if (err) {
-    console.error('Error during adding money to Ivan');
-    }else{
-    console.log('Money has been added to Ivan');
-    Ivan.convertMoney({fromCurrency:'EUR',targetCurrency:'NETCOIN',targetAmount:36000}, (err,data) => {
-    if (err) {
-    console.error('Error during convert money for Ivan')
-    }else{
-    console.log('Money has been converted for Ivan');
-    Ivan.transferMoney({to:'petya',amount:36000},(err,data) => {
-    (err) ? console.error('Error during transfer money to Petya') : console.log(`User Petya transfered money`);
-    });
-    }
-    });
-    }
-    });
-    }
-    });
-    }
+      if (err) {
+        console.error('Error during creating user Ivan')
+      }else{
+        console.log(`User Ivan has been created`);
+        Ivan.performLogin((err,data)=> {
+          if (err) {
+            console.error('Error during login user Ivan');
+          } else {
+            console.log(`User Ivan has been logined`);
+            Ivan.addMoney({ currency:'EUR', amount: 500000 }, (err, data) => {
+              if (err) {
+                console.error('Error during adding money to Ivan');
+              }else{
+                console.log('Money has been added to Ivan');
+                Ivan.convertMoney({fromCurrency:'EUR',targetCurrency:'NETCOIN',targetAmount:36000}, (err,data) => {
+                  if (err) {
+                    console.error('Error during convert money for Ivan')
+                  }else{
+                    console.log('Money has been converted for Ivan');
+                    Ivan.transferMoney({to:'petya',amount:36000},(err,data) => {
+                      (err) ? console.error('Error during transfer money to Petya') : console.log(`User Petya transfered money`);
+                    });
+                  }
+                 });
+              }
+            });
+          }
+        });
+      }
     });
      
     
-    }
+}
      
-    main();
+main();
